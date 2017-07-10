70439a1245a267bfd8d7e1d0388ce7297a9924e1
mminin <mminin2010@gmail.com>
2017-07-10 13:11:31 +0200

    Update FD.py
    Should be good enough for beta testing

--- standalone/FD.py
+++ standalone/FD.py
@@ -1,35 +1,38 @@
-#import scipy
-from PIL import Image
+import sys
 
+sourceFileName = sys.argv[1] # 'Mikhail_test.tif'
+if len(sys.argv)>=4: kx,ky,spc = [int(sys.argv[2]),int(sys.argv[3]),float(sys.argv[4])]
+else:                kx,ky,spc = [3,3,50]
+print(sys.argv[1])
+cMapFileName   = 'CMap.tif'
+FDxFileName    = 'FDx.tif';    FDyFileName    = 'FDy.tif'
+StrikeFileName = 'Strike.tif'; DipFileName    = 'Dip.tif'
+outputFileName = 'output.tif'
+
+from PIL import Image
 import numpy as np
 from scipy import ndimage
 from osgeo import gdal
+import matplotlib.image as mpimg
 radToDegFactor = 57.2958
-
-sourceFileName = 'Mikhail_test.tif'
-kx=3
-ky=3
-spc=50
 
 khx, khy=(kx-1)/2.,(ky-1)/2.
 mx,my=np.meshgrid(np.linspace(-khx,khx,kx),np.linspace(-khy,khy,ky))
 
-#Apply scaling to kernel, see M. Thesis for explanation:
-mx/= spc*kx*khy*(khy+1)*ky 
-my/= spc*ky*khx*(khx+1)*kx
+mx/= spc*kx*khy*(khy+1)*ky # Apply scaling to kernel,
+my/= spc*ky*khx*(khx+1)*kx # see M. Thesis for explanation
 
 sourceImg = gdal.Open(sourceFileName, gdal.GA_ReadOnly)
 array = sourceImg.GetRasterBand(1).ReadAsArray()*1.0
 FDx, FDy = ndimage.convolve(array,mx),ndimage.convolve(array,my)
-
-Image.fromarray(FDx).save('FDx.tif')
-Image.fromarray(FDy).save('FDy.tif')
+Image.fromarray(FDx).save(FDxFileName)
+Image.fromarray(FDy).save(FDyFileName)
 
 Strike = np.arctan2(-FDy,FDx)*radToDegFactor
 Dip = np.arctan(np.sqrt(FDx*FDx+FDy*FDy))*radToDegFactor
 
-Image.fromarray(Strike).save('Strike.tif')
-Image.fromarray(Dip).save('Dip.tif')
+Image.fromarray(Strike).save(StrikeFileName)
+Image.fromarray(Dip).save(DipFileName)
 
-## LAST STEP HERE WOULD BE TO MAKE A COLOURMAP PLOTTER
-
+output=mpimg.imread(cMapFileName)[Dip.astype(int),Strike.astype(int)]
+Image.fromarray(output).save(outputFileName)
